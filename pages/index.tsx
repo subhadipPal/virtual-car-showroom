a4d7538fa10b64d2fe407610bdf14e57e981ee00
Subhadip Pal <subhadip0608@gmail.com>
2022-11-17 16:27:08 +0530

    INSTAMOTION-01 Query param based filtering implemented

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useEffect, useState } from 'react'
+import { useCallback, useContext, useEffect, useMemo, useState } from 'react'
 import { OffersData, Record } from '../typings'
 import { fetchVehicleData, getDefaultAppliedFilters, getFilteredRecords } from '../utils'
 import ProductListingPage from '../components/productListingPage'
@@ -9,22 +9,73 @@ import { useRouter } from 'next/router'
 import { AppContext } from '../context/appContext'
 
 type AppliedFiltersType = { [key: string]: string }
+type QueryParams = {
+  model?: string,
+  make?: string,
+  mileage?: string,
+  power?: string,
+  firstRegistration?: string,
+  fuel?: string,
+  price?: string,
+  gearbox?: string,
+  exteriorColor?: string,
+  category?: string
+}
+
 
 export default function Home({ vehicleData }: { vehicleData: OffersData }) {
   const { getOffersV3Beta: { metadata, records: staticRecords } } = vehicleData
-
+  const router = useRouter()
+  
   const [vehicleOffers, setVehicleOffers] = useState(staticRecords)
   const [filteredVehicleOffers, setFilteredVehicleOffers] = useState<Record[] | undefined>()
   const { toggleOffers,
     appliedFilters: appAppliedFilters } = useContext(AppContext)
-  let areFiltersUpdated = false
+  let areFiltersUpdated = false, aure
   const [appliedFilters, setAppliedFilters] = useState<AppliedFiltersType | undefined>(getDefaultAppliedFilters())
+  const [urlFilterApplied, setUrlFiltersApplied] = useState(false)
 
   if (appAppliedFilters) {
-    areFiltersUpdated = Object.values(appAppliedFilters).some((filterVal) => !filterVal)
+    areFiltersUpdated = Object.values(appAppliedFilters).some((filterVal) => !!filterVal)
   }
-  const router = useRouter()
+  const {
+    model,
+    make,
+    mileage,
+    power,
+    firstRegistration,
+    fuel,
+    price,
+    gearbox,
+    exteriorColor,
+    category }: QueryParams = router.query
 
+    const urlFilters = useMemo(() => ({
+      ...appliedFilters,
+      ...(!!model && { model }),
+      ...(!!make && { make }),
+      ...(!!mileage && { mileage }),
+      ...(!!power && { power }),
+      ...(!!firstRegistration && { firstRegistration }),
+      ...(!!fuel && { fuel }),
+      ...(!!price && { price }),
+      ...(!!gearbox && { gearbox }),
+      ...(!!exteriorColor && { exteriorColor }),
+      ...(!!category && { category })
+    }), [appliedFilters, category, exteriorColor, firstRegistration, fuel, gearbox, make, mileage, model, power, price])
+
+  
+  useEffect(() => {
+    const areUrlFiltersApplied = Object.values(urlFilters)
+    .some((filterVal) => !!filterVal)
+    if (areUrlFiltersApplied && !urlFilterApplied) {
+      const urlFilteredRecords = getFilteredRecords(urlFilters, vehicleOffers)
+      setFilteredVehicleOffers(urlFilteredRecords)
+      setUrlFiltersApplied(true)
+      setAppliedFilters(urlFilters)
+    }
+  }, [urlFilterApplied, urlFilters, vehicleOffers])
+  
   const handleOfferTileClick = useCallback((vehicleId: string) => {
     router.push(`/${vehicleId}`)
     // eslint-disable-next-line react-hooks/exhaustive-deps
